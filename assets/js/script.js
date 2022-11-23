6afcc87bb86e35676ccb856ed8f6535eb34b1863
Bryant Trinh <trinhbryant@gmail.com>
2022-11-23 11:19:23 -0800

    basic html skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1b9acf44bamshbe2aae19de3526fp183346jsndc43cfaa106c',
+		'X-RapidAPI-Host': 'amazon24.p.rapidapi.com'
+	}
+};
+
+fetch('https://amazon24.p.rapidapi.com/api/product?categoryID=aps&keyword=Fullmetal%20Alchemist%20brother&country=US&page=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+  module.exports = {
+  darkMode: 'class',
+  // ...
+}
+
+
