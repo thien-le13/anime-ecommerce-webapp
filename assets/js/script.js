910244dc9c635f04f9016e1e0d4b745e3b5388b0
Buhdurpadur <thien.le13@yahoo.com>
2022-11-23 12:51:14 -0800

    fetch function for names added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1b9acf44bamshbe2aae19de3526fp183346jsndc43cfaa106c',
-		'X-RapidAPI-Host': 'amazon24.p.rapidapi.com'
-	}
-};
+fetch("https://api.jikan.moe/v4/anime/1/full")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
-fetch('https://amazon24.p.rapidapi.com/api/product?categoryID=aps&keyword=Fullmetal%20Alchemist%20brother&country=US&page=1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-
-  module.exports = {
-  darkMode: 'class',
-  // ...
-}
-
-
+//   var animeName =
