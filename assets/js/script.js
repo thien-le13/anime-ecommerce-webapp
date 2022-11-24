04496e6d07cc655632a7898fda24509ed7c98817
Buhdurpadur <thien.le13@yahoo.com>
2022-11-23 18:46:08 -0800

    Anime information when searched by title.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
-fetch("https://api.jikan.moe/v4/anime/1/full")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+var searchInputText = document.querySelector("#anime-title");
+var searchButton = document.querySelector("#search-title-btn");
 
-//   var animeName =
+function getAnime(animeCriteria) {
+  var animeTitleAPI = "https://api.jikan.moe/v4/anime?q=TITLE&sfw";
+  var animeTitle = animeCriteria;
+  fetch(animeTitleAPI.replace("TITLE", animeTitle))
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+function handleTitleSearch() {
+  console.log(searchInputText.value);
+  var searchValue = searchInputText.value;
+  return searchValue;
+}
+
+searchButton.addEventListener("click", function (event) {
+  var anime = handleTitleSearch();
+  getAnime(anime);
+});
