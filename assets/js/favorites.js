37346e404489ffa53e0ef4b73c15a41c99b78d5f
Bryant Trinh <trinhbryant@gmail.com>
2022-11-25 13:14:20 -0800

    structure of favorites page js

---
+++ assets/js/favorites.js
@@ -0,0 +1,87 @@
+// Search Page
+// search => Title
+// returns => Anime obj
+// Input Anime obj in Local Storage
+
+
+// Steps for favorites page:
+
+// parent: animeCard
+
+// Initializing local storage, identify favorite results, loop through local storage, get the current looped obj, 
+
+// create card element, create card div, add image, create that image, 
+
+// Create title div,  
+
+// X icon that clears the favorite anime
+
+
+
+// Favorites Page
+// Initialize function to load Anime objs onto page.
+
+// 
+// [
+// Key:
+// value: {
+//   "title:" "Anime 1 Title,", "image": ."/img/.....png"
+// },
+// {
+//  "title:" "Anime 2 Title,", "image": ."/img/.....png" 
+// }
+// .
+// .
+// .
+// ]
+
+// Initialize
+function getLocalStorage() {
+
+  // identify favorited-results
+  var favoriteResults = document.getElementById("favorited-results");
+}
+
+// loop through local storage
+
+// get current looped obj
+
+// creating card elements
+
+// create anime card div
+
+// add class name to anime card div
+
+// create var for animeImage, create element, set attribute for href, append.
+
+
+// Create img
+  var newImage = document.createElement('img');
+  // <img src="" alt=""/>
+  // <img src="www.anime1.com/image1" alt="X anime name title"/>
+
+
+// Create div for Title, create variable, inner text, classlist.add , appendChild 
+
+
+
+
+// // Clear button
+
+// var clearBtn = document.createElement("button");
+// var closeImage = document.createElement("img");
+
+// clearBtn.classList.add("anime-remove");
+
+// closeImage.setAttribute('src', './x-icon');
+// closeImage.setAttribute('alt', 'Clear favorite anime X button');
+
+// animeCard.appendChild(clearBtn);
+// clearBtn.appendChild(closeImage);
+
+
+// // <button class="anime-remove">
+//   <img src="./x-icon" alt="Clear favorite anime X button" />
+//   </button>
+
+
