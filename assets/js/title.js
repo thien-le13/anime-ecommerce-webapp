aa0b73d028f7359ba7f1ec693a9d90369bfd2449
Steph T <wytseng@uci.edu>
2022-11-28 14:14:48 -0800

    add favorite button html js & changed search genre to get random & reorginze result card structure

--- assets/js/title.js
+++ assets/js/title.js
@@ -2,7 +2,7 @@ var searchInputText = document.querySelector("#anime-title");
 var searchButton = document.querySelector("#search-title-btn");
 
 function getAnime(animeCriteria) {
-  var animeTitleAPI = "https://api.jikan.moe/v4/anime?q=TITLE&sfw";
+  var animeTitleAPI = "https://api.jikan.moe/v4/top/anime?filter=airing";
   var animeTitle = animeCriteria;
   fetch(animeTitleAPI.replace("TITLE", animeTitle))
     .then(function (response) {
@@ -23,6 +23,28 @@ searchButton.addEventListener("click", function (event) {
   var anime = handleTitleSearch();
   getAnime(anime);
 });
+
+// Toggle favorite button
+var favoriteBtn = document.getElementById('favorite-button');
+
+function toggleFavoriteAnime() {
+  let favoriteIcon = document.querySelector('.fa-solid.fa-star');
+  let notFavoriteIcon = document.querySelector('.fa-regular.fa-star');
+
+  if (favoriteIcon.classList.contains('collapse')) {
+    // is favorited 
+    favoriteIcon.classList.remove('collapse');
+    notFavoriteIcon.classList.add('collapse');
+    favoriteBtn.getElementsByTagName('p')[0].innerHTML = 'Favorited';
+  } else {
+    // unfavorite
+    favoriteIcon.classList.add('collapse');
+    notFavoriteIcon.classList.remove('collapse');
+    favoriteBtn.getElementsByTagName('p')[0].innerHTML = 'Favorite';
+  }
+}
+
+favoriteBtn.addEventListener('click', toggleFavoriteAnime);
 
 // Transition dropdown caret 
 var resultHeader = document.getElementById('result-header');
