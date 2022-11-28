4d9f7783beeb035838cf330a279b747c7c30736e
Buhdurpadur <thien.le13@yahoo.com>
2022-11-27 16:00:58 -0800

    simple format

--- assets/js/genre.js
+++ assets/js/genre.js
@@ -1,90 +1,92 @@
-var url = "https://api.jikan.moe/v4/anime/1" // gives anime by id
-var test = "https://api.jikan.moe/v4/anime?q=TITLE&sfw"  // gives anime by title
-var genreURL = "https://api.jikan.moe/v4/genres/anime" // gives genre list
-var genreAnimeURL = "https://api.jikan.moe/v4/anime?genres="  // correct link
+var url = "https://api.jikan.moe/v4/anime/1"; // gives anime by id
+var test = "https://api.jikan.moe/v4/anime?q=TITLE&sfw"; // gives anime by title
+var genreURL = "https://api.jikan.moe/v4/genres/anime"; // gives genre list
+var genreAnimeURL = "https://api.jikan.moe/v4/anime?genres="; // correct link
 
 const animeReturnCount = 5;
 
 var genreDB = [];
 
-var genreDropDown = document.querySelector('#anime-genre');
-var genreSearch = document.querySelector('#search-genre-btn');
+var genreDropDown = document.querySelector("#anime-genre");
+var genreSearch = document.querySelector("#search-genre-btn");
 
-genreSearch.addEventListener('click', function(event){
-    if (genreDropDown.options[genreDropDown.selectedIndex].getAttribute("data-id") === '0')
-    {
-        event.target.blur();
-        return;
-    }
+genreSearch.addEventListener("click", function (event) {
+  if (
+    genreDropDown.options[genreDropDown.selectedIndex].getAttribute(
+      "data-id"
+    ) === "0"
+  ) {
+    event.target.blur();
+    return;
+  }
 
-    fetch(genreAnimeURL + genreDropDown.options[genreDropDown.selectedIndex].getAttribute("value")) // fetch based on genreURL and id of genre
-    .then(function(response){
-        return response.json();
-    }).then(function(data){
-        console.log(data);
-        StoreSearchData(data.data);
-        
+  fetch(
+    genreAnimeURL +
+      genreDropDown.options[genreDropDown.selectedIndex].getAttribute("value")
+  ) // fetch based on genreURL and id of genre
+    .then(function (response) {
+      return response.json();
     })
-    
+    .then(function (data) {
+      console.log(data);
+      StoreSearchData(data.data);
+    });
 });
 
+function GetGenres() {
+  // Fetches array of genre data
+  fetch(genreURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      genreDB = data;
 
-
-function GetGenres()  // Fetches array of genre data
-{
-    fetch(genreURL).then(function(response){
-        return response.json();
-    }).then(function(data){
-        genreDB = data;
-        
-        GenerateGenreDropdown();
+      GenerateGenreDropdown();
     });
 }
 
-function GenerateGenreDropdown()  // Generate genre drop down options
-{
-    for (var i =0; i < genreDB.data.length; i++)
-    {
-        var opt = document.createElement('option');
-        opt.setAttribute('value', genreDB.data[i].mal_id);
-        opt.textContent = genreDB.data[i].name;
+function GenerateGenreDropdown() {
+  // Generate genre drop down options
+  for (var i = 0; i < genreDB.data.length; i++) {
+    var opt = document.createElement("option");
+    opt.setAttribute("value", genreDB.data[i].mal_id);
+    opt.textContent = genreDB.data[i].name;
 
-        genreDropDown.appendChild(opt);
-    }
+    genreDropDown.appendChild(opt);
+  }
 }
 
-// store search results in an array to be accessed later, 
-function StoreSearchData(data){    // Note: Parameter data should be an array that stores objects with info on individual anime
-    var searchResults = [];         
+// store search results in an array to be accessed later,
+function StoreSearchData(data) {
+  // Note: Parameter data should be an array that stores objects with info on individual anime
+  var searchResults = [];
 
-    for (var i = 0; i < animeReturnCount; i++)
-    {
-        var anime = {
-            title: data[i].title,
-            image: data[i].images.jpg.image_url,
-            synopsis: data[i].synopsis
-        }
-        searchResults.push(anime);
-    }
-    // TestImageLink(searchResults[0].image, searchResults[0].synopsis);
-    console.log(searchResults);
+  for (var i = 0; i < animeReturnCount; i++) {
+    var anime = {
+      title: data[i].title,
+      image: data[i].images.jpg.image_url,
+      synopsis: data[i].synopsis,
+    };
+    searchResults.push(anime);
+  }
+  // TestImageLink(searchResults[0].image, searchResults[0].synopsis);
+  console.log(searchResults);
 }
 
 // DEBUG: Function
-function TestImageLink(url, _text)  
-{
-    var image = document.createElement('img');
-    image.setAttribute('src', url);
-    image.setAttribute('alt', 'Test Anime Image');
-    document.querySelector('main').appendChild(image);
+function TestImageLink(url, _text) {
+  var image = document.createElement("img");
+  image.setAttribute("src", url);
+  image.setAttribute("alt", "Test Anime Image");
+  document.querySelector("main").appendChild(image);
 
-    var text = document.createElement('p');
-    text.textContent = _text;
-    document.querySelector('main').appendChild(text);
+  var text = document.createElement("p");
+  text.textContent = _text;
+  document.querySelector("main").appendChild(text);
 }
 
 // BELOW: Functions that must be run on page load
 
 GetGenres();
 // console.log(GetAnimeMerch('naruto'));
-
--- assets/js/title.js
+++ assets/js/title.js
@@ -1,6 +1,7 @@
 var searchInputText = document.querySelector("#anime-title");
 var searchButton = document.querySelector("#search-title-btn");
 
+// Retrieves anime information based on user search.
 function getAnime(animeCriteria) {
   var animeTitleAPI = "https://api.jikan.moe/v4/anime?q=TITLE&sfw";
   var animeTitle = animeCriteria;
@@ -11,7 +12,23 @@ function getAnime(animeCriteria) {
     .then(function (data) {
       getSearchData(data.data);
     });
-}
+  }
+  
+// Takes retrieved information and selects specific data.
+  function getSearchData(data) {
+    var searchResults = [];
+  
+    for (var i = 0; i < animeReturnCount; i++) {
+      var anime = {
+        title: data[i].title,
+        image: data[i].images.jpg.image_url,
+        synopsis: data[i].synopsis,
+      };
+      searchResults.push(anime);
+    }
+    console.log(searchResults);
+    return searchResults;
+  }
 
 function handleTitleSearch() {
   console.log(searchInputText.value);
@@ -19,22 +36,9 @@ function handleTitleSearch() {
   return searchValue;
 }
 
+// Searches for anime after title is entered and search button is clicked.
 searchButton.addEventListener("click", function (event) {
   var anime = handleTitleSearch();
   getAnime(anime);
 });
 
-function getSearchData(data) {
-  var searchResults = [];
-
-  for (var i = 0; i < animeReturnCount; i++) {
-    var anime = {
-      title: data[i].title,
-      image: data[i].images.jpg.image_url,
-      synopsis: data[i].synopsis,
-    };
-    searchResults.push(anime);
-  }
-  console.log(searchResults);
-  return searchResults;
-}
